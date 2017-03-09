9914edeaf15727df5c507433cccf005ff7d09366
Milena Tomova <kokolina1888@gmail.com>
2017-03-09 21:08:01 +0200

    sinc

--- 15_test_prep/group2/task02.php
+++ 15_test_prep/group2/task02.php
@@ -1,8 +1,26 @@
 <?php 
 
 function matrix($rows, $cols){
+	$num = 7;
+	$arr = [];
+	for ($i=0; $i < $rows; $i++) { 
+		for ($j=0; $j < $cols; $j++) { 
+			$arr[$i][$j] = $num;
+			$num = $num + 110;
+		}
+		$num = 7;
+		$num = $num + 7*($i+1);
 
-
+	}
+	echo "<table border=1>";
+	for($m = 0; $m < $rows; $m++){
+		echo "<tr>";
+		for($n = 0; $n < $cols; $n++){
+			echo "<td>" .$arr[$m][$n] . "</td>";				
+		}
+		echo "</tr>";
+	}
+	echo "</table>";
 }
 
-matrix(4, 5);
+matrix(6, 15);
