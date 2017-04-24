3a9a5526cf10d1bc6aeae6611eaf741377a1d47a
Milena Tomova <kokolina1888@gmail.com>
2017-04-24 17:24:46 +0300

    sinc

--- wp_dev/wordpress/wordpress/wp-content/themes/wp_dev/index.php
+++ wp_dev/wordpress/wordpress/wp-content/themes/wp_dev/index.php
@@ -1,46 +1,21 @@
-            <?php get_header(); ?>
-            <div class="col col23">
-                <h2>Welcome To Beauty Class</h2>
-                <p><em>Aenean rutrum, velit in tincidunt ullamcorper, augue arcu cursus tortor, a pulvinar urna eros ut dolor. In mollis augue non dolor faucibus et porta magna congue. </em></p>
-                <p>Beauty Class is free template provided by templatemo for your personal or commercial websites. Please tell your friends about <strong>templatemo.com</strong> website. Thank you. Vestibulum in mauris id nulla fermentum congue nec vel dolor. Duis ultricies, felis eget ornare adipiscing, erat libero iaculis metus, at suscipit metus erat viverra est. Etiam faucibus aliquet turpis sit amet congue. Praesent at nulla id lectus mattis consequat. Validate <a href="http://validator.w3.org/check?uri=referer" rel="nofollow"><strong>XHTML</strong></a> &amp; <a href="http://jigsaw.w3.org/css-validator/check/referer" rel="nofollow"><strong>CSS</strong></a>.</p>
-                <div class="cleaner h40"></div>
-                <h3>Customer Support</h3>
-                <img src="<?php echo get_template_directory_uri(); ?>/images/templatemo_image_01.jpg" alt="" class="img_fl img_border" />
-                <h4>Phasellus sed massa sit amet</h4>
-                <p>Sed quam felis, aliquam non tempor quis, pulvinar feugiat lacus. Nullam a urna ac metus ultrices cursus. Curabitur sed massa libero. Proin ut tortor ipsum et.</p>
-                <a href="#">More</a>
-                <div class="cleaner h40"></div>
-                <h3>Testimonials</h3>
-                <img src="<?php echo get_template_directory_uri(); ?>/images/templatemo_image_02.jpg" alt="" class="img_fl img_border" />
-                <h4>Integer nec arcu nisi eget</h4>
-                <p>Fusce id elit dolor, sit amet lacinia purus. Donec iaculis ullamcorper ipsum id tristique. Donec eget nisl tortor, eu congue felis. Ut blandit auctor odio nec viverra.</p>
-                <a href="#">More</a>
-            </div>
-            <div class="col col13 no_margin_right">
-                <h2>Services</h2>
-                <p><em>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut metus arcu, vitae congue purus.</em></p>
-                <p>Sed massa lectus, pretium in consequat non, sagittis quis nisl. Aenean quis enim lorem. Cras bibendum magna quis urna dapibus convallis. Sed at lorem elit.</p>
-                <ul class="tmo_ul_list">
-                    <li class="first"><a href="#">Sed eget purus</a></li>
-                    <li><a href="#">Vestibulum eleifend</a></li>
-                    <li><a href="#">Nulla odio ipsum</a></li>
-                    <li><a href="#">Suspendisse posuere</a></li>
-                    <li><a href="#">Nunc a dui sed</a></li>
-                    <li><a href="#">Curabitur ac mauris</a></li>
-                    <li><a href="#">Mauris nulla tortor</a></li>
-                    <li><a href="#">Nullam ultrices</a></li>
-                </ul>
-                <div class="cleaner h40"></div>
-                <div id="newsletter">
-                    <h4>Newsletter</h4>
-                    <p>Vestibulum sit amet tincidunt sit amet, tincidunt at dolor. </p>
-                    <form action="#" method="get">
-                      <input type="text" value="Subscribe" name="keyword" id="keyword" title="keyword" onfocus="clearText(this)" onblur="clearText(this)" class="txt_field" />
-                      <input type="submit" name="Subscribe" value=" Subscribe " alt="Subscribe" id="subscribe_btn" title=" Subscribe " class="subscribe_btn"  />
-                      <div class="cleaner"></div>
-                  </form>
-                  <div class="cleaner"></div>
-              </div>
-          </div>
-          <div class="cleaner"></div>
-      <?php get_footer(); ?>
+<?php get_header(); ?>
+<div class="col col23">
+
+    <?php 
+    if( have_posts() ): 
+    while( have_posts() ):
+                     
+
+        the_post();
+     ?>
+        <h2><?php the_title(); ?></h2>
+        <div><?php the_content(); ?></div>
+        <?php   
+      
+        endwhile;
+          endif;
+        ?>
+
+    </div>
+    <div class="cleaner"></div>
+    <?php get_footer(); ?>
